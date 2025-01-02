51bcd0e4c20858a5df4f7717f30878ef8bd11508
Bhavani Mottike <bhavanimo@infinite.com>
2025-01-02 20:23:38 +0530

    mvc assessment

---
+++ MVC Assessments/2nd/Controllers/MoviesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirst.Models;
+using CodeFirst.Repository;
+namespace CodeFirst.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IMovieRepository movieRepo;
+
+        public MoviesController()
+        {
+            movieRepo = new MovieRepository();
+        }
+
+        // Display all movies
+        public ActionResult Index()
+        {
+            var movies = movieRepo.GetAll();
+            return View(movies);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Create a new movie
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movieRepo.Create(movie);
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movie = movieRepo.GetById(id); 
+            if (movie == null)
+            {
+                return HttpNotFound(); 
+            }
+            return View(movie); 
+        }
+        // Edit a movie
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movieRepo.Edit(movie);
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+    
+        
+
+        // Display movies by year
+        public ActionResult MoviesByYear(int year)
+        {
+            var movies = movieRepo.GetAllMoviesByYear(year);
+            return View(movies);
+        }
+    }
+}
---
+++ MVC Assessments/MVCAssessment/MVCAssessment/Controllers/CodeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MVCAssessment.Models;
+namespace MVCAssessment.Controllers
+{
+    public class CodeController : Controller
+    {
+
+        northwindEntities db = new northwindEntities();
+
+        public ActionResult Index()
+
+        {
+
+            return View();
+
+        }
+
+        public ActionResult Customers()
+
+        {
+
+            var customer1 = db.Customers.Where(c => c.Country == "Germany").ToList();
+
+            return View(customer1);
+
+        }
+
+        public ActionResult CustomerOfOrderID()
+
+        {
+
+            var customer2 = db.Orders.Where(o => o.OrderID == 10248).Select(o => o.Customer).FirstOrDefault();
+
+            return View(customer2);
+
+        }
+
+    }
+
+}
